806826832761aa8ad8eb6d7a07d0d7391fd48636
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-09-08 17:14:19 +0300

    Simplified XML

--- vocabsMaker.js
+++ vocabsMaker.js
@@ -14,10 +14,10 @@ for (let v in vocabsJSON) {
 }
 
 function toXML(obj) {
-    let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<tns:vocabulary xmlns=\"http://einfracentral.eu\" xmlns:tns=\"http://einfracentral.eu\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://einfracentral.eu https://builds.openminted.eu/job/eic-registry-model/ws/target/generated-resources/schemagen/schema1.xsd\">\n";
+    let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<vocabulary xmlns=\"http://einfracentral.eu\">\n";
     for (let i in obj) {
         ret += util.format("\t<%s>%s</%s>\n", i, obj[i], i);
     }
-    ret += "</tns:vocabulary>";
+    ret += "</vocabulary>";
     return ret;
 }
