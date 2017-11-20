39eedfe823118ff743f09603c4fdac5ba522be74
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-20 16:27:56 +0200

    fix id generation

--- transformers/vocabulary.js
+++ transformers/vocabulary.js
@@ -20,7 +20,7 @@ let writeVocabulary = ([type, content]) => {
 let toXML = (obj) => {
     let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<vocabulary xmlns=\"http://einfracentral.eu\">\n";
     for (let i in obj) {
-        ret += util.format("\t<%s>%s</%s>\n", i, (i === "id" ? (obj.type + " " + obj[i] ).replace(/\s/g, "_") : obj[i]), i);
+        ret += util.format( "\t<%s>%s</%s>\n", i, obj[i], i);
     }
     ret += "</vocabulary>";
     return ret;
