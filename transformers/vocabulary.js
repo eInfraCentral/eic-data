2c5cd36d76fcae44b55e8796953b300c9c5e105d
Panagiotis Lampropoulos <plambros@otenet.gr>
2017-11-20 12:03:53 +0200

    split vocabs output

--- transformers/vocabulary.js
+++ transformers/vocabulary.js
@@ -2,24 +2,28 @@
  * Created by pgl on 24/7/17.
  */
 let infile = "../raw/vocabularies.json";
-let outdir = "../transformed/vocabulary.res";
+let outdir = "../transformed";
 let util = require("util");
 let fs = require("fs");
 let vocabsJSON = require(infile);
-for (let v in vocabsJSON) {
-    vocabsJSON[v].forEach(function (e) {
-        fs.writeFileSync(
-            util.format("%s/%s-%s.xml", outdir, v, e.id).replace(/\s/g, "_"),
-            toXML(Object.assign(e, {type: v})),
-            "utf8"
-        );
-    });
-}
-function toXML(obj) {
+let writeVocabulary = ([type, content]) => {
+    let d = util.format("%s/%s.res", outdir, type.toLowerCase());
+    if (!fs.existsSync(d)) {
+        fs.mkdirSync(d);
+    }
+    content.forEach(e => fs.writeFile(
+        util.format("%s/%s.xml", d, e.id.replace(/\s/g, "_")),
+        toXML(Object.assign(e, {type})),
+        "utf8"
+    ));
+};
+let toXML = (obj) => {
     let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<vocabulary xmlns=\"http://einfracentral.eu\">\n";
     for (let i in obj) {
         ret += util.format("\t<%s>%s</%s>\n", i, (i === "id" ? (obj.type + " " + obj[i] ).replace(/\s/g, "_") : obj[i]), i);
     }
     ret += "</vocabulary>";
     return ret;
-}
+};
+let main = () => Object.entries(vocabsJSON).forEach(writeVocabulary);
+main();
