13e657dfaea363e631df9ad9cc03d2db7e7e79a5
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-01-31 13:18:04 +0200

    +safety

--- exports/dumpJasons.sh
+++ exports/dumpJasons.sh
@@ -3,7 +3,7 @@
 #machine="localhost"
 #machine="vereniki.athenarc.gr"
 #machine="dl105.madgik.di.uoa.gr"
-machine="beta.einfracentral.eu"
+#machine="beta.einfracentral.eu"
 
 base="http://${machine}:8080/eic-registry/"
 query="/by/id"
@@ -15,3 +15,4 @@ do
     url="${base}${resourceType}${query}"
     response=$(curl ${url} -o ${target}/${resourceType}.json)
 done < resourceTypes.txt
+
--- exports/postXmls.sh
+++ exports/postXmls.sh
@@ -2,7 +2,7 @@
 
 #machine="localhost"
 #machine="vereniki.athenarc.gr"
-machine="dl105.madgik.di.uoa.gr"
+#machine="dl105.madgik.di.uoa.gr"
 #machine="beta.einfracentral.eu"
 
 base="http://${machine}:8080/eic-registry/"
