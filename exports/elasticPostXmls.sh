8f869a2929760b2133a29acda8f448b3a9baca68
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-02 18:42:01 +0200

    +elastic post

---
+++ exports/elasticPostXmls.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source="xmls"
+target=$1
+base="http://${target}:8080/eic-registry/"
+
+postAllResourcesForType() {
+    for file in $(ls ./${source}/$1); do
+        postResource $1 ${file}
+    done
+}
+postResource() {
+    cmd="curl -v --silent --output /dev/null --write-out %{http_code} --trace-ascii curl_trace.txt --data @./${source}/${1}/${2} --header Content-Type:application/xml ${base}${1}"
+    response=$(${cmd})
+    if ((${response} >= 200 && ${response} < 300 )); then
+        colors="\e[32m"
+    else
+        colors="\e[31m"
+    fi
+    if ((${response} < 200 || ${response} >= 300 )); then
+        printf "[${colors}%3d\e[0m] Posting [%9s] to %9s --> %s\n" "${response}" "${2}" "${target}" "$1"
+        read input </dev/tty
+    fi
+}
+postAllResources() {
+    for resourceType in $(ls ./${source}); do
+        postAllResourcesForType ${resourceType}
+    done
+}
+postAllResources
+wait
