5df82c8c8bb1be76ef070846190255cb62facd82
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-02 18:37:56 +0200

    +elastic scripts

---
+++ exports/elasticConvertJasonsToXmls.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+let fs = require("fs");
+let util = require("util");
+let shared = require("../transformers/shared.js");
+
+let source = "elasticJasons";
+let target = "xmls";
+
+let main = async () => {
+    let resourceTypes = (await shared.read("resourceTypes.txt", 'utf8')).split("\n");
+    await shared.mkdir(util.format("%s/%s/", __dirname, target));
+    for (let i = 0; i < resourceTypes.length - 1; i++) {
+        let type = resourceTypes[i];
+        let count = 0;
+        let hitsOfType = require(util.format("%s/%s/%s.%s", __dirname, source, type, "json")).hits.hits;
+        await shared.mkdir(util.format("%s/%s/%s/", __dirname, target, type));
+        for (let hit of hitsOfType) {
+            let filename = util.format("%s/%s/%s/%s.%s", __dirname, target, type, hit._source.id, "xml");
+            let content = hit._source.payload;
+            await shared.write(filename, content, "utf8");
+            count++;
+        }
+        console.log(util.format("Converted %s resources of type %s", count, type));
+    }
+};
+
+main();
---
+++ exports/elasticDumpJasons.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source=$1
+target="elasticJasons"
+
+base="http://${source}:9200/"
+query="/_search?size=1000"
+
+mkdir -p ${target}
+
+while read resourceType
+do
+    resourceTypeName=${resourceType}
+    if [ "$resourceTypeName" == "user" ]; then
+        resourceTypeName=einfrauser
+    fi
+    url="${base}${resourceTypeName}${query}"
+    echo "Fetching resources of type ${resourceType} from ${url}"
+    response=$(curl -s ${url} -o ${target}/${resourceType}.json)
+done < resourceTypes.txt
