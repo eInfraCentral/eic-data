e167521bafe60e3c7335124fd8c5a03f2bb332d6
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-02 18:36:48 +0200

    now using shared.js

--- transformers/service.js
+++ transformers/service.js
@@ -1,26 +1,23 @@
 /**
  * Created by pgl on 31/10/17.
  */
-let infile = process.argv[2];
-let separator = process.argv[3] || "$";
-console.log({infile, separator});
-let outdir = "../transformed/service.res";
+
 let fs = require("fs");
 let util = require("util");
+let shared = require("shared.js");
+
+let infile = process.argv[2];
+let separator = process.argv[3] || "$";
+
+let outdir = "../transformed/service.res";
+
 let fields = ["id", "url", "name", "tagline", "description", "options", "targetUsers", "userValue", "userBase", "symbol",
     "multimediaURL", "providers", "version", "lastUpdate", "changeLog", "validFor", "lifeCycleStatus", "trl", "category",
     "subcategory", "places", "languages", "tags", "requiredServices", "relatedServices", "request", "helpdesk", "userManual",
     "trainingInformation", "feedback", "price", "serviceLevelAgreement", "termsOfUse", "funding", "serviceAddenda"];
 let services = [];
 let field = 0;
-function toXML(obj) {
-    let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<service xmlns=\"http://einfracentral.eu\">\n";
-    for (let i in obj) {
-        ret += util.format("\t<%s>%s</%s>\n", i, obj[i], i);
-    }
-    ret += "</service>";
-    return ret;
-}
+
 function insert(line) {
     let tokens = line.split(separator);
     for (let i = 0; i < tokens.length; i++) {
@@ -31,9 +28,9 @@ function insert(line) {
     }
     field++;
 }
-function writeOut() {
+async function writeOut() {
     for (let s in services) {
-        fs.writeFileSync(util.format("%s/%s.xml", outdir, services[s].id), toXML(services[s]), "utf8");
+        await shared.write(util.format("%s/%s.xml", outdir, services[s].id), shared.toXML(services[s], "service"), "utf8");
     }
 }
 require("readline").createInterface({input: fs.createReadStream(infile)}).on("line", insert).on("close", writeOut);
--- transformers/vocabulary.js
+++ transformers/vocabulary.js
@@ -1,29 +1,27 @@
+#!/usr/bin/env node
+
 /**
  * Created by pgl on 24/7/17.
  */
-let infile = "../raw/vocabularies.json";
-let outdir = "../transformed";
+
 let util = require("util");
 let fs = require("fs");
+let shared = require("./shared");
+
+let infile = "../raw/vocabularies.json";
+let outdir = "../transformed";
 let vocabsJSON = require(infile);
-let writeVocabulary = ([type, content]) => {
+
+let writeVocabulary = async ([type, content]) => {
     let d = util.format("%s/%s.res", outdir, type.toLowerCase());
-    if (!fs.existsSync(d)) {
-        fs.mkdirSync(d);
-    }
-    content.forEach(e => fs.writeFile(
+    await shared.mkdir(d);
+    content.forEach(async e => await shared.write(
         util.format("%s/%s.xml", d, e.id.replace(/\s/g, "_")),
-        toXML(Object.assign(e, {type})),
+        shared.toXML(Object.assign(e, {type}), "vocabulary", type),
         "utf8"
     ));
 };
-let toXML = (obj) => {
-    let ret = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<vocabulary xmlns=\"http://einfracentral.eu\">\n";
-    for (let i in obj) {
-        ret += util.format( "\t<%s>%s</%s>\n", i, obj[i], i);
-    }
-    ret += "</vocabulary>";
-    return ret;
-};
+
 let main = () => Object.entries(vocabsJSON).forEach(writeVocabulary);
+
 main();
