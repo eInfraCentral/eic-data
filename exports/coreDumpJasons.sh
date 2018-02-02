66ce65c5ad81ce110d5ab4cfe3963d6f25978eb4
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-02 18:40:17 +0200

    now skipping vocabs in core mode

---
+++ exports/coreDumpJasons.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source=$1
+target="jsons"
+
+base="http://${source}:8080/eic-registry/"
+query="/by/id"
+
+mkdir -p ${target}
+
+while read resourceType
+do
+    if [ "$resourceType" == "vocabulary" ]; then
+        echo "Skipping vocabulary"
+    else
+        url="${base}${resourceType}${query}"
+        echo "Fetching resources of type ${resourceType} from ${url}"
+        response=$(curl -s ${url} -o ${target}/${resourceType}.json)
+    fi
+done < resourceTypes.txt
