2ed0472a420ccde7c60bc1d121bf6642a58be015
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-02-02 18:41:53 +0200

    now skipping vocabs in core mode

--- exports/corePostXmls.sh
+++ exports/corePostXmls.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-#machine="localhost"
-#machine="vereniki.athenarc.gr"
-#machine="dl105.madgik.di.uoa.gr"
-#machine="beta.einfracentral.eu"
-
-base="http://${machine}:8080/eic-registry/"
 source="xmls"
+target=$1
+base="http://${target}:8080/eic-registry/"
 
 postAllResourcesForType() {
     for file in $(ls ./${source}/$1); do
@@ -21,14 +17,18 @@ postResource() {
     else
         colors="\e[31m"
     fi
-    printf "[${colors}%3d\e[0m] Posting [%9s] to %9s --> %s\n" "${response}" "${2}" "${machine}" "$1"
     if ((${response} < 200 || ${response} >= 300 )); then
+        printf "[${colors}%3d\e[0m] Posting [%9s] to %9s --> %s\n" "${response}" "${2}" "${target}" "$1"
         read input </dev/tty
     fi
 }
 postAllResources() {
-    for dir in $(ls ./${source}); do
-        postAllResourcesForType ${dir}
+    for resourceType in $(ls ./${source}); do
+        if [ "$resourceType" == "vocabulary" ]; then
+            echo "Skipping vocabulary"
+        else
+            postAllResourcesForType ${resourceType}
+        fi
     done
 }
 postAllResources
