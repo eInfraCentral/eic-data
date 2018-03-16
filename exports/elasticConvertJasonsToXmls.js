e295e3806f6437f4b727f7339af23087b4942353
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-03-16 17:55:52 +0200

    added protection for absent resource types

--- exports/elasticConvertJasonsToXmls.js
+++ exports/elasticConvertJasonsToXmls.js
@@ -11,17 +11,21 @@ let main = async () => {
     let resourceTypes = (await shared.read("resourceTypes.txt", 'utf8')).split("\n");
     await shared.mkdir(util.format("%s/%s/", __dirname, target));
     for (let i = 0; i < resourceTypes.length - 1; i++) {
-        let type = resourceTypes[i];
-        let count = 0;
-        let hitsOfType = require(util.format("%s/%s/%s.%s", __dirname, source, type, "json")).hits.hits;
-        await shared.mkdir(util.format("%s/%s/%s/", __dirname, target, type));
-        for (let hit of hitsOfType) {
-            let filename = util.format("%s/%s/%s/%s.%s", __dirname, target, type, hit._source.id, "xml");
-            let content = hit._source.payload;
-            await shared.write(filename, content, "utf8");
-            count++;
+        try {
+            let type = resourceTypes[i];
+            let count = 0;
+            let hitsOfType = require(util.format("%s/%s/%s.%s", __dirname, source, type, "json")).hits.hits;
+            await shared.mkdir(util.format("%s/%s/%s/", __dirname, target, type));
+            for (let hit of hitsOfType) {
+                let filename = util.format("%s/%s/%s/%s.%s", __dirname, target, type, hit._source.id, "xml");
+                let content = hit._source.payload;
+                await shared.write(filename, content, "utf8");
+                count++;
+            }
+            console.log(util.format("Converted %s resources of type %s", count, type));
+        } catch (err) {
+            console.error(err);
         }
-        console.log(util.format("Converted %s resources of type %s", count, type));
     }
 };
 
